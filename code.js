5d7cf6648cb44f2ac19c3e5dc9a276017af817e3
breyes4 <71798440+breyes4@users.noreply.github.com>
2021-03-01 20:00:55 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,49 +1,42 @@
-$(document).ready(function () {
-        // add event listener
-        $("form#gradebook").submit(submitGrade);
+$(document).ready(
+    function () {
+        $("#submit_student").click(submitDetails);
 
-        // add workable function
-        function submitGrade(event) {
+        function submitDetails(event) {
             event.preventDefault();
 
-            let firstName = $("#firstName").val();
-            let lastName = $("#lastName").val();
-            let earnedPoints = $("#pointsEarned").val();
-            let possiblePoints = $("#pointsPossible").val();
+            var studentGrade =
+                {
+                    firstName: $("#first_name").val(),
+                    lastName: $("#last_name").val(),
+                    pointsEarned: parseInt($("#points_earned").val()),
+                    pointsPossible: parseInt($("#points_possible").val()),
+                }
 
-            let student = {};
-            student.first = firstName;
-            student.last = lastName;
-            student.earned = earnedPoints;
-            student.possible = possiblePoints;
-            student.percent = Math.round(student.earned / student.possible * 100);
+            var finalGrade = studentGrade.pointsEarned/studentGrade.pointsPossible*100;
+            var letterGrade = "";
 
-            let letterGrade = "";
-
-            if (student.percent <= 100 && student.percent >= 90) {
+            if (finalGrade >= 90){
                 letterGrade = "A";
             }
-            else if (student.percent < 90 && student.percent >= 80) {
+            else if (finalGrade >= 80 && finalGrade < 90){
                 letterGrade = "B";
             }
-            else if (student.percent < 80 && student.percent >= 70) {
+            else if (finalGrade >= 70 && finalGrade <80){
                 letterGrade = "C";
             }
-            else if (student.percent < 70 && student.percent >= 60) {
+            else if (finalGrade >= 60 && finalGrade < 70) {
                 letterGrade = "D";
             }
-            else if (student.percent < 60 && student.percent >= 0) {
+            else
+            {
                 letterGrade = "F";
             }
-            else {
-                (alert("Please input valid number!"));
-                return;
-            }
 
-
-            $("p#output").append(`${student.first} ${student.last} ${student.percent}`);
-            $("p#letterGrade").text(`Your grade is a(n) ${letterGrade}`);
+            $("#first").text("First Name: " + studentGrade.firstName);
+            $("#last").text("Last Name: " + studentGrade.lastName);
+            $("#percentage").text("Score: " + finalGrade);
+            $("#letter").text("Grade: " + letterGrade);
         }
-
     }
-);
+)
